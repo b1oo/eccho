d8dddae8fbb3930dc0b5ac950147b0fb85ae410f
Sergio <sergiobluez@gmail.com>
2024-06-19 03:24:47 -0700

    desktop analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div className="App" style={{ height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <iframe 
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-60LB1M3NXF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-60LB1M3NXF');
+</script>
+<iframe 
         src="https://ffm.to/rn2qo8j"
         title="Embedded Content"
         style={{ width: '100%', height: '100%', border: 'none' }}
